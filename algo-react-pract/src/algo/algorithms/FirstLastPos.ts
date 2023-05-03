db20a87a859a09a2e7b947d591391a40d6f32a91
adobecrack-cc-2022 <86190550+kenjie-sama@users.noreply.github.com>
2023-05-04 00:44:56 +0800

    Fixed the deficiencies of React Packages

---
+++ algo-react-pract/src/algo/algorithms/FirstLastPos.ts
@@ -0,0 +1,12 @@
+const arr: number[] = [9, 6, 8, 8, 1, 8, 4, 2, 7, 2, 7];
+const target: number = 2;
+export const FirstLastPos = (): void => {
+  const newArr: number[] = arr.sort();
+  let start: number = -1,
+    end: number = -1;
+  arr.forEach((el) => {
+    console.log(`${el} ${typeof el}`);
+    if (el == target) start = el;
+  });
+  console.log(start);
+};
